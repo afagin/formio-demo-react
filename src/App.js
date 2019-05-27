deee9fbf88550e8a9f93b198ffd842b343dc23ef
afagin <andrewfagin912@gmail.com>
2019-05-27 14:16:50 -0700

    add formio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Form } from 'react-formio';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Form io test</h1>
+      <Form src="https://ijbmwmdiwoxzjat.form.io/abc/def" />
     </div>
   );
 }
