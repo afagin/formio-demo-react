4ed69526d23a54ed47364a159f6d5278ff0a0fa9
afagin <andrewfagin912@gmail.com>
2019-05-29 14:22:15 -0700

    add enter url link

--- src/components/render-form.js
+++ src/components/render-form.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from 'react-router-dom';
 import FormLoader from "./form-loader";
 import { submitForm } from "../api";
 
@@ -16,6 +17,9 @@ class RenderForm extends Component {
 
     return (
       <div className="form-renderer">
+        <Link className="btn-link" to="/enter">
+          Enter Url
+        </Link>
         <FormLoader url={formUrl} handleSubmit={this.handleSubmit} />
       </div>
     );
